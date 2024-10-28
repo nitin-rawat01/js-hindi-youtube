d9f7a6597ec0bb6ac9086afbccf8b658d95779de
nitin-rawat01 <nitrawat2499@gmail.com>
2024-10-28 06:02:43 +0000

    dataTypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+// Datatypes and ECMA Standards
+//  "use strict": literalls :treat all js code as newer version
+//js engine will treat entire code as per new standards.: "use strict"
+
+//alert in node
+//alert(3+3) // we are using nodejs, not browser
+
+//writing code: space, semi-colon etc.
+//js ignore extra space
+console.log(3 +
+    3
+) //code readibility that should be high
+
+console.log('hitesh');
+
+//documentation: mdn(written by mozilla)
+//original documentation: tc39.es
+//ecma orginaztion: define standards 
+
+//data type: global object, numbers and data.
+
+//Example of data types
+let name = "hitesh" //string
+let age = 28 // Number
+let isloggedIn = true // boolean
+let state; // undefined
+
+//data types in javascript
+//primitive data types
+//number => 2^53
+//bigInt // trading, stockmarket, reddit, facebook
+//string: use double quote to define string: ""
+//boolean => true/false: detailsAvailabe, isloggedIn , response from server
+
+//null: standalone value, also a data type // representation of empty value//we intentionally define null/empty
+//undefined =>  let state: variable declare, value is not defined but null is represenation of empty value. 
+// symbol: mostly used in react and figma. In react it is used to define uniqueness of the component. In short for uniqueness.(unique)
+
+
+// Object
+
+console.log(typeof null); // output: object: some called it as a error in a language.
+console.log(typeof undefined); // output: undefined: because undefined is type in javascript
+
