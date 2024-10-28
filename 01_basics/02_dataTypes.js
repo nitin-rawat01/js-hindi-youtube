9f3a6d9f88dba5173dd6807f3892f842b964e4bd
nitin-rawat01 <nitrawat2499@gmail.com>
2024-10-28 06:28:06 +0000

    datatypes and ecma script

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -42,3 +42,38 @@ let state; // undefined
 console.log(typeof null); // output: object: some called it as a error in a language.
 console.log(typeof undefined); // output: undefined: because undefined is type in javascript
 
+//it change the way js is executed it changes to use strict operating context.
+/* 
+The "use strict" directive was new in ECMAScript version 5.
+
+It is not a statement, but a literal expression, ignored by earlier versions of JavaScript.
+
+The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+
+With strict mode, you can not, for example, use undeclared variables.
+When strict mode is enabled, the JavaScript engine enforces additional constraints. This means you may be able to catch some common errors that would have otherwise gone unnoticed. It also helps you write cleaner and more secure code.
+*/
+// "use strict";
+// (function doWork(){
+//     "use strict";
+//     x = 10;
+//     console.log(x);
+// })();
+
+// console.log(x);
+//without using "use strict" x will end as a global variable
+//you can set either entire program to strict mode or scope it to specific function 
+//"use strict" at the top of script file or at function body
+//"use strict":prevents
+// prevent using variable property or an object without declaring it is not allowed
+//deleting a variable, funciton, or an argument, is not allowed 
+    // x = 3.14
+    //delete x; cause an error
+//defining a property more than once is not allowed
+    // eg var x = {p1: 10, p1:20}; cause an error.
+    
+//duplicating a parameter name,
+//function (x,x); cause an error
+
+//octal numeric literals and escape characters are not allowed:
+
