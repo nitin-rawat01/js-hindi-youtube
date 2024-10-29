ba62dfe9321a5bc93232f27811d8d01509ff2017
nitin-rawat01 <nitrawat2499@gmail.com>
2024-10-29 10:44:12 +0000

    something about memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -48,3 +48,22 @@ console.log(typeof myObj); //object
 console.log(typeof heroes); // object
 console.log(typeof id); //symbol
 
+////////////////
+//memory and thred: diff and complex but not that much
+//how memory works in javascript
+//automatic garbase collection
+//memory allocation in javascript
+//two types of memory: stack(primitive) , heap(non-primitive type)
+/*
+Primitive dataType goes in stack memory; And it provides copy of that element.
+So when we change something (through assigned variable) it gets changed in copy of element  not in original.
+
+
+Opposite of this. Non primitive dataType get stored in HEAP memory. 
+And it gives reference value to assigned elements. 
+So when assigned variable makes changes it changes the both values(original and assigned one). 
+*/
+
+
+
+
