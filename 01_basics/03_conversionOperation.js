808a5ab8a4f919989832cb60b8a4f4ef13be871b
nitin-rawat01 <nitrawat2499@gmail.com>
2024-11-08 07:49:13 +0000

    revision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,59 +1,112 @@
 // conversion and operation.
-// let score = "33" //
-// let score = "33abc" //not a pure number
-// let score = null // convert into 0
-// let score = undefined // convert it into nan
-// let score = true // convert it into 0
-let score = "hitesh" // nan
+//converting string into number
+// let score = "33" // string will easily convert into number
 
+//Cases when conversion not happen succesfully
+// converting not pure number into number
+// let score = "33abc" //not a pure number 
+    // note: javascript still convert 33abc into number for number conversion
+    // note: but it's value in NaN(not a number)
+    // note: type: number and value: NaN
+    // be carefull when dealing with nan
 
-//sometime we dont know the type and format  of the value we recieve.
-console.log(typeof score)
-console.log(typeof(score))
+//converting null into number in javascript
+// let score = null 
+    //note: js convert null into 0 for number conversion
+    //type: number and value: 0
 
-//conversion
-//data type: capital format
-//class based
-let valueInNumber = Number(score); //string to number
-console.log(typeof valueInNumber);
-console.log(valueInNumber) //nan:not a number (special type) 
-//note: be careful with javascript because it will convert strint/not pure number into number type but value will be nan(not a number); 
+//converting undefined into number in javascript
+// let score = undefined 
+    // note: we got value of NaN
+    // type: number and value: NaN
 
-/* notes: basic conversion
+//converting boolean value into number
+// let score = true 
+    // note: in case of true value, converted into 1
+    // note: value is 1 and type is number and vice-versa for false
+
+//converting string value into number
+let score = "hitesh" 
+    //note:in case of string , value is NaN
+    //note: value is NaN, type is Number
+
+    /* summar: basic conversion
  "33" => 33
  "33abc" => value: NaN, type: Number
  true/false => 0/1
 */
 
+
+//sometime we dont know the type and format  of the value we recieve.
+console.log(typeof score) 
+console.log(typeof(score))
+
+//conversion
+//data type: capital format
+//Number: class based
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber) //nan:not a number (special type) 
+
+//note: be careful with javascript because it will convert strint/not pure number into number type but value will be nan(not a number); 
+
+
+
 //conversion: value into boolean
+//direct boolean value
 // let isloggedIn = true; //direct boolen value
 
-// let isloggedIn = 1; //number to boolean : 1 => true, 0 => false
-// let isloogedIn = ""; // false
-let isloggedIn = "hitesh"; // true
+//converting number into boolean
+//we have Boolean for conversion into boolean value same as we have Number and String
+// let isloggedIn = 1; 
+    // note: 1 is converted into true in boolean, 0 is converted into false
+    // number to boolean : 1 => true, 0 => false
 
+//converting empty string into boolean
+// let isloogedIn = ""; 
+    //note: empty string is converted into false
+    //note:value of empty string in boolean is false
 
-let booleanIsLoggedIn = Boolean(isloggedIn);
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
+//converting  string into boolean
+let isloggedIn = "hitesh"; 
+ //note:  string is converted into true
+    //note:value of string in boolean is true
 
-/* notes
+/* notes: summary
  1  => true; 0 => false converting 1 into boolean
  empty string => false, string =>  true
 */
 
-// conversion: string
-let someNumber  = 33
+let booleanIsLoggedIn = Boolean(isloggedIn);
 
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+
+
+// conversion: string
+//number into string
+// let someNumber  = 33
+    //note: converted into 33 as a string
+
+//number into boolean
+let someNumber = true;
+    // note: converted into true as a string
+
+//browser give value in string-  we need to convert it into boolean, number , object and array.
+
+//converting diff value into string
 let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
 // -----Operations---------------------------
 let value = 3;
 let negValue = -value;
 console.log(negValue);
+
 //Basic operations(Arithmatic Operations)
 // console.log(2+2);
 // console.log(2-2);
@@ -65,7 +118,7 @@ console.log(negValue);
 //operations on string
 let str1 = "hello";
 let str2 = " hitesh";
-let str3 = str1 + str2;
+let str3 = str1 + str2; //+ use to concatenate string
 console.log(str3); //hello hitesh
 
 //problem area
