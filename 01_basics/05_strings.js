c337b50c968d4d6aa772ac223bc60fe2c79fdced
nitin-rawat01 <nitrawat2499@gmail.com>
2024-11-23 10:43:48 +0000

    String concept revision with exta notes

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,5 @@
-// use modern syntax of string.
+// use modern syntax of string: use template literals when using string
+//no difference between using single or double quote.
 const name = "hitesh" //declaring string //behind the scene: invoke object
 const repoCount = 50
 
@@ -10,19 +11,20 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //mod
 //back tick -> string interpolation ->use placeholder(inject variable)
 
 //second method for declaring string
-
-const gameName = new String('Hitesh-hc'); //new-> create object
+//use javascript object
+const gameName = new String('Hitesh-hc'); //new-> create object, String ->contructor
+//same value ...behind the scene- invoke object.
 // also run this on chrome dev tools
 console.log(gameName); //return-> String{'hitesh'} 0:h ,1:i(key:value pair) , length property , lots of diff method inside prototye, type: object.
 
-// console.log(gameName[0]); //h
-// console.log(gameName.__proto__); //access prototype //return -> {} empty object(but not empty) run this on dev tools
+console.log(gameName[0]); //h
+console.log(gameName.__proto__); //accessing prototype of object //return -> {} empty object(but not empty) run this on dev tools
 
 
 // console.log(gameName.length); 
 //all the methods inside prototype directly..don't need to write __proto__
 //String Methods
-console.log(gameName.toUpperCase()); //HITESH original string doesnot change because reason mention in stack and heap
+console.log(gameName.toUpperCase()); //HITESH original string doesnot change because reason mention in stack and heap video
 
 //to find which char is at which position //pass number
 console.log(gameName.charAt(2)); //t at second index
@@ -62,7 +64,69 @@ const lastString = 'hitesh-hc-com';
 //split into diff part based on - split(separtor, limit);
 console.log(lastString.split('-')); // output[ 'hitesh', 'hc', 'com' ]
 
+/* revision: Extra Notes
+// Strings in Javascript
+//two ways to declare string in javascript
+//first: usign string literals and template literals
+//second: string constructor
+let name = "hitesh";
+let gameName = new String('hitesh-hc');
+console.log(name); //type: string
+console.log(gameName); //type: object
+console.log(gameName.__proto__);//accessing prototype of object
 
+//various property and methods of string
+//length property of string: return no. of char
+console.log(gameName.length);
+
+//accessign particular character of string using index.
+console.log('char from index value:', gameName[7]); //h
+//using charAt();
+console.log('char at method:', gameName.charAt(7)); //h
+
+//upper-lower case
+console.log( 'Upper case:', gameName.toUpperCase());
+console.log('Lower Case' ,gameName.toLowerCase());
+
+//getting index of character
+//indexOf:return index position of first matching value
+//return -1 incase of value not found
+console.log('Index value of t: ', gameName.indexOf('t'));
+
+//divide string into sub-string or extract part of string
+//syntax: substr(start, length)
+console.log('substr() method:', gameName.substr(2,4));
+
+//divide string or extract part of string 
+//slice(start, end); end position does not include;
+//A negative number selects from the end of the string.
+//slice method is also available for array
+console.log(gameName.slice(4,6));
+console.log([2,3,55,23,4].slice(2,4)); 
+
+//includes: check whether sub-string is part of string or not
+//return boolean
+console.log(gameName.includes('nas'));
+
+//converting string into array
+//convert full string into array of single element
+console.log('string into array', gameName.split());
+console.log('Array of individual chars:', gameName.split(''));
+//join method convert array into string
+//reverse method reverse array
+
+//remove white space from start and end of string: trim
+//there are lot of other trim methods:trimStart();
+gameName = "    hitesh-hc  ";
+console.log('without trim:', gameName.trimRight())
+console.log('with trim:', gameName.trim());
+
+//replace method: replace particular part of stirng with another string
+//replace first matching string
+//replace('toReplaceString', 'replacewith');
+const url = "https://hitesh.com/hitesh%20choudary";
+console.log(url.replace('20', '-'))
+*/
 
 
 
