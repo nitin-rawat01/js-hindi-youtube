dc123eb8b6deddb68679a5dbf3722b82be789c45
nitin-rawat01 <nitrawat2499@gmail.com>
2024-12-18 10:36:28 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,13 +19,13 @@ const regularUser = {
     }
 }
 // Accessing Values: dot notation
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 //Optional Chaining:purpose: provide protection , usecase: when we receive response from api
 // how it works: if fullname exist then go for userfullname...help us to avoid if-else
-console.log(regularUser.fullname?.userfullname.firstname);
+// console.log(regularUser.fullname?.userfullname.firstname);
 
-console.log(tinderUser); //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+// console.log(tinderUser); //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 //Merging Objects
 const obj1 = {1: "a", 2:"b"}
@@ -43,12 +43,12 @@ const obj4 = {5:"a", 6: "b"};
 const obj3 = Object.assign({}, obj1, obj2); //copy all the values to empty objects //read documentation // since target is an empty object obj1 and obj2 will remain same.
 const obj5 = Object.assign({}, obj1, obj2, obj4); 
 
-console.log(obj3);
-console.log(obj1);
+// console.log(obj3);
+// console.log(obj1);
 
 //third method: spread operator
 const obj6 = {...obj1, ...obj2};
- console.log(obj6);
+//  console.log(obj6);
 
 
 
@@ -72,22 +72,84 @@ const obj6 = {...obj1, ...obj2};
  users[1].email;
 
 
- console.log(tinderUser); //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+//  console.log(tinderUser); //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 //objects methods : Very Important
 //getting all the keys of an objects
-console.log(Object.keys(tinderUser)); //return array's keys of object
+// console.log(Object.keys(tinderUser)); //return array's keys of object
 //now we can apply loop to the array //[ 'id', 'name', 'isLoggedIn' ]
 
 //getting all the values
-console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser));
 
 //getting all the key-value pair
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 //to check if object includes the property or not 
-console.log(tinderUser.hasOwnProperty("isLoggedIn")); //true
-console.log(tinderUser.hasOwnProperty("isLogged")); //false
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); //true
+// console.log(tinderUser.hasOwnProperty("isLogged")); //false
 
 //to check more important objects method : create objects on browser console
 //note: you can close the looping of the objects by using some methods 
+
+//Object de-structure and JSON API intro
+//de-structuring of objects 
+// In react: receive object -> need to destructure object -> get values (we can also directly access the values)
+
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+//getting values of object
+console.log(course.courseInstructor);
+
+//note: above approach is bad if we have to use value of courseInstructor multiple times throughout the code. It make our program bulky
+
+//to make code clean we have another syntax
+const {courseInstructor:instructor} = course;
+//now value of courseInstructor is store in instructor variable
+//syntax: const {keytoExtracted:variable name} = object;
+console.log(typeof instructor);
+console.log(course);
+//this is called object destructuring 
+
+//Use of destructuring of objects in react
+//creating props in react
+// const navbar = ({prop}) => { //props by default,  we receive props by default
+
+// }
+// navbar(company = "hitesh")
+
+// API Concepts
+//api: values from backend and how we write
+//before api values: xml structure
+//now api values: json
+// json: {} object but no name but object has name
+//key and value both are string in json
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+//we recieve api in json -> {}
+//but sometime we receive api in array format and we have many objects inside of that array
+[
+    {},
+    {},
+    {},
+    {},
+]
+//we have to spend time to understand the response from the api..or we can say it takes time to understand json
+//website: json formatter : help us to understand json 
+//for practise go to : randomuser.me -> to format the json /to understand the api -> json formatter
+//json: javascript object notation
+// all the api is in json format
+
+
+
+
+
+
