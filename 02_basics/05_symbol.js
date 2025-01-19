b9f49f79dec9d2d6322905ca155a5bf7e2ac2a5f
nitin-rawat01 <nitrawat2499@gmail.com>
2025-01-19 08:33:42 +0000

    symbol

--- 02_basics/05_symbol.js
+++ 02_basics/05_symbol.js
@@ -18,3 +18,12 @@ console.log(id1 === id2); //false
 
 // Summary: symbol is a primitve unique value with an optional description. 
 
+// Javascript: Symbol
+// Symbols don't auto- convert to a string
+// Most values in javascript support implicit (total/complete) conversion to string. Symbols are special. They don’t auto-convert.
+alert(id); // TypeError: Cannot convert a Symbol value to a string this will work in browser
+// That’s a “language guard” against messing up, because strings and symbols are fundamentally different and should not accidentally convert one into another.
+// If we really want to show a symbol, we need to explicitly call .toString() on it, like here:
+alert(id.toString());  // Symbol(id), now it works
+// get symbol.description property to show the description only:
+alert(id.description); //id
