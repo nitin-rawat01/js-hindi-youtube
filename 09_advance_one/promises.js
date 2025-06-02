364199e4d1b069aeb8bf7d275bc78a23923a400d
nitin-rawat01 <nitrawat2499@gmail.com>
2025-06-02 10:12:20 +0000

    promise

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,19 @@
+// everything in javascript is an object reference/ prototype ref.
+
+// Asynchronous operations
+// : file ascess
+// : some calculation on data
+// : crptography (password encryption)
+// : network call
+// : mobile dev(camera, microphone access).
+
+// promise: operation completed in future.
+
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+// A Promise is in one of these states:
+
+//     pending: initial state, neither fulfilled nor rejected.
+//     fulfilled: meaning that the operation was completed successfully.
+//     rejected: meaning that the operation failed.
+
