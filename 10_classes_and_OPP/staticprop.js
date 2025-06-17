a931f923b06c67a80f77aa4351985a2b75a20cb3
nitin-rawat01 <nitrawat2499@gmail.com>
2025-06-17 09:28:59 +0000

    static properties in class

---
+++ 10_classes_and_OPP/staticprop.js
@@ -0,0 +1,33 @@
+// static properties: static prop
+//Static class methods are defined on the class itself. You cannot call a static method on an object, only on an object class.
+class User {
+    constructor(username){
+        this.username = username;
+    }
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+    // static: stop the access
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User('hitesh');  
+// console.log(hitesh.createId()); //hitesh.createId is not a function
+// situation: where we do not want to give the access to every object instances from this class
+// also work in case extens
+class Teacher extends User {
+    constructor(email, username){
+        super(username);
+        this.email = email;
+    }
+
+
+}
+
+const iphone = new Teacher('iphone', 'i@phone.com');
+console.log(iphone);
+iphone.logMe();
+console.log(iphone.createId());
+// note: static -> no object and class can use or inherit the properties /funcitons.
