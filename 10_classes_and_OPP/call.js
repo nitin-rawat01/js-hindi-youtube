e7f8a84244710237508a61faf704e93a005bcdfb
nitin-rawat01 <nitrawat2499@gmail.com>
2025-06-17 08:03:00 +0000

    call and this in javascript

--- 10_classes_and_OPP/call.js
+++ 10_classes_and_OPP/call.js
@@ -1,3 +1,4 @@
+// video: call and this in javascript
 // call, bind and apply
 // Notes:
 // callstack: The call stack in JavaScript is a mechanism that keeps track of function calls during the execution of a program. It operates on a "Last In, First Out" (LIFO) principle, which means the last function that is called is the first one to finish executing. 
@@ -14,13 +15,24 @@
 // call keyword practical example
 function setUsername(username){
     // complex DB calls
-    this.username = username;
+    this.username =username;
+    // return this.username = username; why this will not work: solution in chatgpt
 }
 
 function createUser(username, email, password){
-    
-    setUsername(username);
+    // setUsername(username): after called, it's execution context from callstack will also removed and all the  properties also removed(gone).
+    // we need to hold the reference of that funciton(setUsername(username))
+    // to hold reference of the function we use method: .call
+    // we need those values..we need to give execution context...apne this ki value de rhe hai.
+    // setUsername(username);
+    setUsername.call(this, username);
     this.email = email;
     this.password = password;
     
 }
+
+// call keyword: Calls a method of an object, substituting another object for the current object.
+
+const chai =new createUser("chai", "chai@fb.com", "12334");
+console.log(chai)
+// hamra current execution context kisi or function ko pass kr deta hai.
