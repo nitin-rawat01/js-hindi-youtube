bf7e53a6fc71bdce8446067e48e1ecb49cd01f4e
nitin-rawat01 <nitrawat2499@gmail.com>
2025-06-22 14:43:21 +0000

    getter and setter in object

---
+++ 10_classes_and_OPP/object_get_set.js
@@ -0,0 +1,26 @@
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase();
+    },
+    set email(value){
+        this._email = value;
+    }
+
+}
+
+
+// Object.create -> factory function 
+const tea = Object.create(User);
+console.log(tea.email);
+tea.email = "hitesh@gmail.com"
+console.log(tea.email);
+
+// _ -> define private property
+// get and set use  krne ke baad _ ka meaning nhi rheta ..ab wo dekha ki apke pas email property 
+// getter and setter special method jo property ke upr rhkh rha hun..
+// memory se value lekar arhe ho ya dekar arhe ho is process ko overwrite krne ke liye getter or setter hai.
+// get email: get ek special keyword hai jo is property(email) per work kr rha hai
+// example: length property of array
