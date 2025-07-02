16586b4ea4f67c71c34c7f2777a3f54e8799d38a
nitin-rawat01 <nitrawat2499@gmail.com>
2025-07-02 11:50:51 +0000

    first solution for race condition.

---
+++ 12_RaceCondition/solutionOne.js
@@ -0,0 +1,22 @@
+// 1. Promises and async/await for Sequential Execution:
+let balance = 100;
+
+async function withdraw(amount){
+    if(balance >= amount){
+        await new Promise(resolve => setTimeout(resolve, 50));  // Simulate async check
+        balance -= amount;
+        console.log(`Withdraw ${amount}, new balance ${balance}`);
+    } else {
+        console.log(`Cannot withdraw ${amount}. Insufficient balance.`)
+    }
+}
+
+async function runTransactions() {
+     withdraw(70);
+     withdraw(50);
+     withdraw(50);
+
+    console.log(`Final balance: ${balance}`);
+}
+
+runTransactions();
