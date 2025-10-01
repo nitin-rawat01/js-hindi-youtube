39ced0f1fd0f9dd725eb64e6bd11edf664b51c92
nitin-rawat01 <nitrawat2499@gmail.com>
2025-10-01 04:33:51 +0000

    WeakMap

---
+++ 14_Maps/WeakMap.js
@@ -0,0 +1,8 @@
+// Garbage Collection
+// JavaScript employs a memory management mechanisms knwon as Garbage Collection
+// The primary functions are:
+    // Ensuring efficient use of memory storage
+    // Reclaim memory occupied by variables that are no longer use.
+    // Preventing memory leaks
+
+
